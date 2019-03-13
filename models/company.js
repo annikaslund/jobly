4219a039a303389c28c980b133050740800ca021
Chengsi <gaochengsi@gmail.com>
2019-03-13 11:07:34 -0700

    add company model and add get route

--- models/company.js
+++ models/company.js
@@ -1,1 +1,17 @@
-module.exports = router;
+const db = require('../db');
+
+/** Company of the site */
+class Company {
+    /** all() returns a list of companies
+     * {handle, name} for all of the company objects
+     */
+    static async all() {
+        const result = await db.query(
+            `SELECT handle, name
+                FROM companies`
+        );
+        return result.rows;
+    }
+}
+
+module.exports = Company;
--- routes/companies.js
+++ routes/companies.js
@@ -1,6 +1,18 @@
-// const express = require("express");
-// const router = new express.Router();
-// const User = require("../models/user");
-// const ExpressError = require("../expressError");
+const express = require("express");
+const router = new express.Router();
+const Company = require("../models/Company");
+const ExpressError = require("../expressError");
 
-// module.exports
+/** GET a list of companies with {handle, name} */
+router.get('/', async function(req, res, next){
+    try{
+        const companies = await Company.all();
+        return res.json({ companies });
+    } catch (err) {
+        return next(err);
+    }
+})
+
+
+
+module.exports = router;
