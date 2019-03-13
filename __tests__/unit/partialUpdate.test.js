423498f8fe74343522dc06bf5e54678d07109bf6
annika <annikaslund@gmail.com>
2019-03-13 10:37:04 -0700

    Write unit test for sqlForPartialUpdate()

--- __tests__/unit/partialUpdate.test.js
+++ __tests__/unit/partialUpdate.test.js
@@ -1,9 +1,15 @@
+process.env.NODE_ENV = "test";
+const sqlForPartialUpdate = require("../../helpers/partialUpdate")
+
+// const app = require("../app");
+
+// const db = require("../db");
+
 describe("partialUpdate()", () => {
   it("should generate a proper partial update query with just 1 field",
       function () {
-
-    // FIXME: write real tests!
-    expect(false).toEqual(true);
-
+    expect(sqlForPartialUpdate('testCompany', {'testKey': 'testValue'}, 'testKey', 1))
+      .toEqual({'query': `UPDATE testCompany SET testKey=$1 WHERE testKey=$2 RETURNING *`,
+            'values': ['testValue', 1]})
   });
 });
