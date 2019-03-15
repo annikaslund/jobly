0ae32e43b5eae6b2d97b525e66d494e6b3e73f53
annika <annikaslund@gmail.com>
2019-03-15 08:37:31 -0700

    Add jobSchema.json and update docstrings on routes/jobs

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = new express.Router();
 const Job = require("../models/Job");
+const jsonschema = require("jsonschema");
+const jobSchema = require("../schemas/jobSchema.json");
 
-/** GET a list of filtered jobs with {handle, name} */
+/** GET /jobs returns a list of filtered jobs as 
+ * jobs: [{handle, name}, {handle, name}, ...] */
 router.get('/', async function(req, res, next){
     try{
         //get search term if there is any
@@ -14,12 +17,21 @@ router.get('/', async function(req, res, next){
     }
 })
 
-/** POST a job and return the new job data */
+/** POST /jobs creates a new job and returns the new job data as 
+ * job: {title, salary, equity, company_handle, date_posted}*/
 router.post('/', async function(req, res, next){
     try{
-        let jobData = req.body;
         //add job with jsonschema validation
-        let job = await Job.create(jobData);
+        const result = jsonschema.validate(req.body, jobSchema);
+
+        if (!result.valid) {
+            //pass validation errors to error handler
+            let listOfErrors = result.errors.map(error => error.stack);
+            let error = new ExpressError(listOfErrors, 400);
+            return next(error)
+        }
+
+        let job = await Job.create(req.body);
         return res.json({ job })   
     } catch (err) {
         return next(err);
